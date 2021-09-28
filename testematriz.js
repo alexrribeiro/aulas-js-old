ae8712173d7de4b6b01c2da1f80847f40526e0d9
alexrribeiro <alexramosribeiro@gmail.com>
2021-09-28 11:40:30 -0300

    Exemplos de array e outras alterações

--- testematriz.js
+++ testematriz.js
@@ -1,37 +1,57 @@
+//Declaração do Array
 var marcas = ["Chevrolet", "Volkswagen", "Fiat", "Renault", "Hyundai"];
 
 function teste(){
+    // Exibição do conteúdo do array
     document.write("<p>Conteúdo do array: ")
     document.write(marcas);
 
-    document.write("</p><p>")
-    document.write(marcas[4]);
-    document.write("</p>");
-
-    marcas.push("Toyota");
-    marcas.unshift("Honda");
-    document.write("<p>Conteúdo do array: ")
-    document.write(marcas);
-
-    marcas.sort();
-    document.write("<p>Conteúdo do array: ")
-    document.write(marcas);
-    
+    // Exibição 2º elemento do array
     document.write("</p><p>");
-    document.write(marcas[4]);
+    document.write(marcas[2]); // Fiat
     document.write("</p>");
+
+    // Insere Toyota no final e Honda no início
+    marcas.unshift("Honda");
+    marcas.push("Toyota");
+    document.write("<p>Conteúdo do array: ")
+    document.write(marcas);
+
+    // Ordena em ordem alfabética
+    marcas.sort();
+    // marcas.reverse();
+    document.write("<p>Conteúdo do array: ")
+    document.write(marcas);
     
+    // // Exibição do 4º elemento do array
+    // document.write("</p><p>");
+    // document.write(marcas[4]); // Renault
+    // document.write("</p>");
+    
+    // Busca e remove Renault do array
     var buscar = "Renault";
     var indice = marcas.indexOf(buscar);
     marcas.splice(indice, 1);
 
-    document.write(indice);
+    // document.write(indice);
     document.write("<p>Conteúdo do array: ")
     document.write(marcas);
 
+    // Remove o primeiro e o último elemento do array
     marcas.shift();
     marcas.pop();
     document.write("<p>Conteúdo do array: ")
     document.write(marcas);
 
+    // Exibe o tamanho do array
+    var tamanho = marcas.length;
+    document.write("<p>Tamanho do array: ")
+    document.write(tamanho);
+    document.write("</p>");
+
+    // Apaga todos os elementos
+    marcas.splice(0, tamanho);
+    document.write("<p>Conteúdo do array: ")
+    document.write(marcas);
+    document.write("</p>");
 }
